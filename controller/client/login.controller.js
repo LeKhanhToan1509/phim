eca75a78fc3552efd0799ea365584d0cd26dce23
LeKhanhToan1509 <toanxm1509@gmail.com>
2024-04-04 04:21:27 +0700

    last

--- controller/client/login.controller.js
+++ controller/client/login.controller.js
@@ -1,59 +1,45 @@
-const bcrypt = require("bcryptjs");
 const account = require("../../models/Account.model.js");
-
-const asyncHandler = (fn) => (req, res, next) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
-
-const sendResponse = (res, status, url, message) => {
-    res.status(status).redirect(
-        url + "?message=" + encodeURIComponent(message)
-    );
-};
 
 module.exports.login = (req, res) => {
     res.render("client/pages/login.pug");
 };
 
-module.exports.postLogin = asyncHandler(async (req, res) => {
+module.exports.postLogin = async (req, res) => {
     const { email, password } = req.body;
-
     try {
-        const user = await account.findOne({ email });
-        if (user && (await bcrypt.compare(password, user.password))) {
+        const user = await account.findOne({
+            email: email,
+            password: password,
+        });
+        if (user) {
             res.cookie("token", user.token, { httpOnly: true });
             res.redirect(`/`);
         } else {
-            sendResponse(
-                res,
-                401,
-                "/login",
-                "Your login details are incorrect"
-            );
+            res.status(401).redirect("/login");
         }
     } catch (error) {
         console.error("Login error:", error);
-        sendResponse(res, 500, "/login", "Server error occurred");
+        res.redirect("/login?error=Server error occurred");
     }
-});
+};
 
 module.exports.register = (req, res) => {
     res.render("client/pages/register.pug");
 };
 
-module.exports.postRegister = asyncHandler(async (req, res) => {
+module.exports.postRegister = async (req, res) => {
     const email = req.query.email;
-    res.render("client/pages/register2.pug", { email });
-});
-
-module.exports.userRegister = asyncHandler(async (req, res) => {
-    const { password, ...otherUserData } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new account({ ...otherUserData, password: hashedPassword });
+    res.render("client/pages/register2.pug", {
+        email: email,
+    });
+};
+module.exports.userRegister = async (req, res) => {
+    const user = new account({ ...req.body });
     await user.save();
     res.redirect("/account/register/password/step2");
-});
+};
 
-module.exports.step2 = (req, res) => {
+module.exports.step2 = async (req, res) => {
     res.render("client/pages/step2.pug");
 };
 
--- controller/middleware/auth.middleware.js
+++ controller/middleware/auth.middleware.js
@@ -1,4 +1,3 @@
-const jwt = require("jsonwebtoken");
 const account = require("../../models/Account.model");
 
 module.exports.requireAuth = async (req, res, next) => {
@@ -6,20 +5,17 @@ module.exports.requireAuth = async (req, res, next) => {
         if (!req.cookies.token) {
             return res.redirect(`/account/login`);
         }
-        const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-        const userId = decoded.id;
-
-        const user = await account.findById(userId).select("-password");
+        const token = req.cookies.token;
+        const user = await account
+            .findOne({ token: token })
+            .select("-password");
         if (!user) {
-            return res.redirect(`/account/login`);
+            return res.redirect(`account/login`);
+        } else {
+            res.locals.user = user;
         }
-        res.locals.user = user;
         next();
     } catch (error) {
-        if (error.name === "JsonWebTokenError") {
-            return res.redirect("/account/login?error=Invalid token");
-        }
-
         console.error(error);
         res.redirect("/account/login?error=Server error occurred");
     }
