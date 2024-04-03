3a0218459070d595528b61723bc38fa247b69ce7
LeKhanhToan1509 <toanxm1509@gmail.com>
2024-04-04 01:40:43 +0700

    add

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -13,12 +12,10 @@ app.use(methodOverride("_method"));
 app.use(cookieParser());
 // Your route requires
 const Route = require("./routes/client/index.route");
-const RouteAdmin = require("./routes/admin/index.route");
 
 // Database and system configuration
 const port = process.env.PORT;
 const connect = require("./config/database.js");
-const systemConfig = require("./config/system.js");
 connect.connection();
 
 Route(app);
@@ -26,8 +23,6 @@ Route(app);
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 app.use(express.static(`${__dirname}/public`));
-
-app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
