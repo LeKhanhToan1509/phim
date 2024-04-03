a18541950586ed3b213bbf66167420e70f85f890
LeKhanhToan1509 <toanxm1509@gmail.com>
2024-04-04 04:23:17 +0700

    last

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,18 +1,13 @@
 const Film = require("../../models/Film.model");
 
 module.exports.index = async (req, res) => {
-    try {
-        const [initialFilms, moreFilms] = await Promise.all([
-            Film.find({}).skip(0).limit(5),
-            Film.find({}).skip(5).limit(100),
-        ]);
+    const [initialFilms, moreFilms] = await Promise.all([
+        Film.find({}).skip(0).limit(5),
+        Film.find({}).skip(5).limit(100),
+    ]);
 
-        res.render("client/pages/home.pug", {
-            films: initialFilms,
-            allFilms: moreFilms,
-        });
-    } catch (error) {
-        console.error("Error fetching films:", error);
-        res.status(500).send("An internal error occurred");
-    }
+    res.render("client/pages/home.pug", {
+        films: initialFilms,
+        allFilms: moreFilms,
+    });
 };
