32d9e9dc18b04bcc7762893fc0b90223b69eee4c
LeKhanhToan1509 <toanxm1509@gmail.com>
2024-04-05 12:46:53 +0700

    last

--- models/Account.model.js
+++ models/Account.model.js
@@ -1,28 +1,38 @@
 const mongoose = require("mongoose");
 const { generateString } = require("../helper/gen.js");
 
-const accountSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-    like: [
-        {
-            id: String,
-            likeAt: Date,
+const accountSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            default: "you",
         },
-    ],
-    lastWatch: [
-        {
-            id: String,
-            lastAt: Date,
+        email: String,
+        password: String,
+        like: [
+            {
+                id: String,
+                likeAt: Date,
+            },
+        ],
+        lastWatch: [
+            {
+                id: String,
+                lastAt: Date,
+            },
+        ],
+        token: {
+            type: String,
+            default: generateString(10),
         },
-    ],
-    token: {
-        type: String,
-        default: generateString(10),
+        image: String,
+        delete: { type: Boolean, default: false },
+        updateAt: Date,
     },
-    image: String,
-    delete: { type: Boolean, default: false },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const account = mongoose.model("account", accountSchema, "accounts");
 module.exports = account;
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,8 @@ const HomeRoutes = require("./home.route");
 const AccountRoutes = require("./account.route");
 const WatchRoutes = require("./watch.route");
 const UserRoutes = require("./user.route");
+const middleware = require("../../controller/middleware/auth.middleware");
+
 module.exports = (app) => {
     app.use("/", HomeRoutes);
     app.use("/account", AccountRoutes);
